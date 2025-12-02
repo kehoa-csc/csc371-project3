9934e3e4ac5cfe61870321f9088348c49bb06b4b
andrewkehoe <157737128+andrewkehoe@users.noreply.github.com>
2025-12-02 12:51:10 -0500

    basic dirt pattern

--- CodeCraftJr/app/src/main/java/com/example/codecraftjr/MainActivity.kt
+++ CodeCraftJr/app/src/main/java/com/example/codecraftjr/MainActivity.kt
@@ -12,16 +12,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.codecraftjr.ui.theme.CodeCraftJrTheme
+import android.content.pm.ActivityInfo
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         enableEdgeToEdge()
         setContent {
             CodeCraftJrTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +43,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    //Game board
+    Row() {
+        for (i in 1..8) {
+            Column() {
+                for (j in 1..6) {
+                    Image(
+                        painter = painterResource(id = R.drawable.dirt),
+                        contentDescription = "dirt",
+                        modifier = Modifier.size(calcBlockSize().dp).border(0.5.dp, color = Color.Gray)
+                    )
+                }
+            }
+        }
+    }
+    //Bottom nav bar
+}
+@Composable
+fun calcBlockSize(): Int {
+    val width = LocalConfiguration.current.screenWidthDp
+    val height = LocalConfiguration.current.screenHeightDp
+    if (height < width) {
+        return height/7
+    }
+    return width/9
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device="spec:width=411dp,height=891dp,dpi=420,isRound=false,chinSize=0dp,orientation=landscape")
 @Composable
 fun GreetingPreview() {
     CodeCraftJrTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
