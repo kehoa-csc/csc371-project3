f951bbd899c4653e43ff4a2993f179970efa727b
andrewkehoe <157737128+andrewkehoe@users.noreply.github.com>
2025-12-04 09:51:34 -0500

    right block drag&drop

--- CodeCraftJr/app/src/main/java/com/example/codecraftjr/MainActivity.kt
+++ CodeCraftJr/app/src/main/java/com/example/codecraftjr/MainActivity.kt
@@ -1,31 +1,42 @@
 package com.example.codecraftjr
 
+import android.content.ClipData
+import android.content.ClipDescription
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.codecraftjr.ui.theme.CodeCraftJrTheme
-import android.content.pm.ActivityInfo
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.codecraftjr.ui.theme.CodeCraftJrTheme
+import androidx.compose.foundation.draganddrop.dragAndDropSource
+import androidx.compose.foundation.draganddrop.dragAndDropTarget
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.draganddrop.DragAndDropEvent
+import androidx.compose.ui.draganddrop.DragAndDropTarget
+import androidx.compose.ui.draganddrop.DragAndDropTransferData
+import androidx.compose.ui.draganddrop.mimeTypes
+import androidx.compose.ui.draganddrop.toAndroidDragEvent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,8 +55,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
+    val codebar = remember { mutableStateListOf<String>() }
     //Background
     Scaffold(containerColor = Color.hsl(216F, .84F,.902F)) { }
     Image(
@@ -53,13 +66,12 @@ fun Greeting(modifier: Modifier = Modifier) {
         contentDescription = "background",
         modifier = Modifier.size(LocalConfiguration.current.screenWidthDp.dp)
     )
-
     //Game board
     Row(horizontalArrangement = Arrangement.Center,
     modifier = Modifier.fillMaxSize()) {
-        for (i in 1..8) {
+        repeat(8) {
             Column() {
-                for (j in 1..6) {
+                repeat(6) {
                     Image(
                         painter = painterResource(id = R.drawable.dirt),
                         contentDescription = "dirt",
@@ -72,15 +84,56 @@ fun Greeting(modifier: Modifier = Modifier) {
     //Bottom code bar
     Row(horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.Bottom,
-        modifier = Modifier.fillMaxSize())
-        {
-            for (i in 1..8) {
-                Image(
-                    painter = painterResource(id = R.drawable.stone),
-                    contentDescription = "stone",
-                    modifier = Modifier.size(calcBlockSize().dp).border(0.5.dp, color = Color.Black)
-                )
+        modifier = Modifier.fillMaxSize()
+            .dragAndDropTarget(
+                shouldStartDragAndDrop = {event -> event.mimeTypes().contains(ClipDescription.MIMETYPE_TEXT_PLAIN)},
+                target = remember {
+                    object: DragAndDropTarget {
+                        override fun onDrop(event: DragAndDropEvent): Boolean {
+                            val text: String = event.toAndroidDragEvent().clipData?.getItemAt(0)?.text as String
+                            if (codebar.size<8) {
+                                codebar.add(text)
+                            }
+                            return true
+                        }
+                    }
+                }
+            )
+    ) {
+        for (i in 0..7) {
+            var blockId: Int = R.drawable.stone
+            if (i < codebar.size) {
+                when (codebar[i]) {
+                    "right_arrow" -> blockId = R.drawable.right
+                }
             }
+            Image(
+                painter = painterResource(id = blockId),
+                contentDescription = "stone",
+                modifier = Modifier.size(calcBlockSize().dp).border(0.5.dp, color = Color.Black)
+            )
+        }
+    }
+    Column(horizontalAlignment = Alignment.End,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize().padding(64.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.right),
+            contentDescription = "right block",
+            modifier = Modifier
+                .size(calcBlockSize().dp).border(0.5.dp, color = Color.Black)
+                .dragAndDropSource{
+                    detectTapGestures(
+                        onLongPress = { offset ->
+                            startTransfer(
+                                transferData = DragAndDropTransferData(
+                                    clipData = ClipData.newPlainText("codeblock", "right_arrow")
+                                )
+                            )
+                        }
+                    )
+                }
+        )
     }
 }
 
